281bee85e5279d8a2e4856a6ff6cd1259fa2f55c
26-PM <prafulcoc1@gmail.com>
2025-04-15 22:15:30 +0530

    chore: Update CORS configuration to allow all origins

--- donation-platform-backend/index.js
+++ donation-platform-backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://donateconnect.vercel.app",
+  origin: "*",
   credentials: true // if using cookies or Authorization headers
 }));
 
